34316a2a170d59644d47f38234ac261671c8f16e
viinb <vic.rodrg@gmail.com>
2026-03-04 11:44:01 -0300

    inicializar

---
+++ arrays/ImprimeArgumentos/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ImprimeArgumentos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
---
+++ arrays/Ordena/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Array.Sort(args);
+
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
